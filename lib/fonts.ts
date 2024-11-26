3901395035c332504773d08a39d8780beab6903b
Gema Refantero <96099133+Refara08@users.noreply.github.com>
2024-11-26 22:33:58 +0700

    init shadcn and home page

---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import { Source_Serif_4, Inter } from "next/font/google";
+
+export const inter = Inter({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const source_serif_pro = Source_Serif_4({
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
