17e7aff1b1fe21456217ea6a1f8d8688689dcabc
Gema Refantero <96099133+Refara08@users.noreply.github.com>
2024-12-12 00:46:07 +0700

    feat: dashboard UI

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -1,5 +1,20 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/shadcn/button";
+// import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
+import { AppSidebar } from "@/components/dashboard/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/shadcn/breadcrumb";
+import { Separator } from "@/components/shadcn/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/shadcn/sidebar";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -11,22 +26,38 @@ const DashboardPage = async () => {
   }
 
   return (
-    <div>
-      <h1>Dashboard page</h1>
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/" });
-        }}
-      >
-        <Button type="submit" variant={"destructive"}>
-          Logout
-        </Button>
-      </form>
-      <div>
-        <p>{JSON.stringify(session, null, 2)}</p>
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -11,6 +11,8 @@ export default async function Home() {
 }
 
 // TODOList
-// 1. make schemas for database
-// 2. make authentication
+////1. make schemas for database
+//// 2. make authentication
 // 3. Figuring out TinyMCE upload image
+// 4. update schema database PostSection untuk dynamic content text & image
+// 5. Upload image to cloudinary
