45805ec2357ed9f1db14dc48f56820e9873a82d3
Gema Refantero <96099133+Refara08@users.noreply.github.com>
2026-06-07 17:45:40 +0700

    feat: update hover card on mobile

--- components/home/showcase-card.tsx
+++ components/home/showcase-card.tsx
@@ -84,7 +84,7 @@ const ShowcaseCard = ({
         className,
       )}
     >
-      <div className="group-hover:bg-[#353535] rounded-lg shadow-inner">
+      <div className="group-hover:md:bg-[#353535] rounded-lg shadow-inner">
         <div className={cn("flex justify-between items-center ")}>
           {image_url && (
             <div
@@ -100,7 +100,7 @@ const ShowcaseCard = ({
                   "object-cover size-full md:object-contain object-left-top md:object-center",
                 )}
               />
-              <div className="img_overlay absolute inset-x-0 bottom-0 w-full h-[40px] bg-gradient-to-t from-[#f5f5f5] to-transparent z-10 group-hover:from-[#353535]" />
+              <div className="img_overlay absolute inset-x-0 bottom-0 w-full h-[40px] bg-gradient-to-t from-[#f5f5f5] to-transparent z-10 group-hover:md:from-[#353535]" />
             </div>
           )}
           <div className="text flex-1 text-left pl-2 pr-4">
@@ -108,14 +108,14 @@ const ShowcaseCard = ({
               <h3
                 className={cn(
                   source_serif_pro.className,
-                  "text-sm font-semibold mb-1 group-hover:text-[#ffffff]",
+                  "text-sm font-semibold mb-1 group-hover:md:text-[#ffffff]",
                 )}
               >
                 {title}
               </h3>
             )}
             {description && (
-              <p className="text-[11px] text-[#545A5F] group-hover:text-[#ffffff]">
+              <p className="text-[11px] text-[#545A5F] group-hover:md:text-[#ffffff]">
                 {description}
               </p>
             )}
@@ -125,9 +125,9 @@ const ShowcaseCard = ({
           {categories?.map((category) => (
             <div
               key={category.id}
-              className="bg-white group-hover:bg-[#444444] border border-[#eeeeee] group-hover:border-[#EEEEEE1A] rounded-full py-1 px-2"
+              className="bg-white group-hover:md:bg-[#444444] border border-[#eeeeee] group-hover:md:border-[#EEEEEE1A] rounded-full py-1 px-2"
             >
-              <p className="text-muted-foreground text-xs group-hover:text-white">
+              <p className="text-muted-foreground text-xs group-hover:md:text-white">
                 {category.name}
               </p>
             </div>
